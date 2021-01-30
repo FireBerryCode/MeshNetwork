c01cbd347f04ef726470a7844a124a5d622a2d36
MichaelisTrofficus <32078719+MichaelisTrofficus@users.noreply.github.com>
2021-01-30 22:03:30 +0100

    Actualizar para captura de informacion de todos los sensores

--- receiver_node_network_rpi.py
+++ receiver_node_network_rpi.py
@@ -7,21 +7,28 @@ from RF24Network import *
 """
 Codigo para la Raspberry como Nodo raiz. Este ejemplo espera un struct del tipo
 
-{
+struct payloadRF24 {    
   char id[3];   
-  float temperature; 
-}
+  float temperature;  
+  float humidity;
+  uint16_t visible;
+  uint16_t ir;
+  float lux;
+  int irFlame;
+  int mq2;
+  int mq7;
+};
 
-Habria que cambiarlo para el caso final y modificar la funcion unpack_c_struct(payload)
-El id no hay que cambiarlo porque en principio los ids de los nodos hijos seran siempre 
-01, 02, 03, 04, 05 (se puede establecer comunicacion desde nodos nietos, pero miedo 
-me da)
 """
 
 def unpack_c_struct(payload):
-	id_node_1, id_node_2, _,  temperature = unpack('<3cf', bytes(payload))
+	print(bytes(payload))
+	id_node_1, id_node_2, _,  temperature, humidity, \
+               visible, ir, lux, ir_flame, mq2, mq7 \
+               = unpack('<3cffHHfhhh', bytes(payload))
 	id_node = id_node_1 + id_node_2
-	return id_node, temperature
+	return id_node, temperature, humidity, visible, ir, \
+           lux, ir_flame, mq2, mq7
 
 radio = RF24(22, 0)
 network = RF24Network(radio)
@@ -37,7 +44,7 @@ other_node = octlit("01")
 
 radio.begin()
 time.sleep(0.1)
-network.begin(90, this_node)    # channel 90
+network.begin(120, this_node)
 radio.printDetails()
 packets_sent = 0
 last_sent = 0
@@ -45,10 +52,20 @@ last_sent = 0
 while 1:
     network.update()
     while network.available():
+        print('===========================')
         print("Network available")
-        header, payload = network.read(8)
+        header, payload = network.read(25)
         print("payload length ", len(payload))
-        id_node, temperature = unpack(payload)
-        print('Temperature ', temperature, ' ºC at node ', id_node)
+        measure = unpack_c_struct(payload)
+        print('ID: ', measure[0])
+        print('Temperature: ', measure[1])
+        print('Humidity: ', measure[2])
+        print('Visible Light: ', measure[3])
+        print('Infrared Light: ', measure[4])
+        print('Lux: ', measure[5])
+        print('Flame Detector: ', measure[6])
+        print('MQ2: ', measure[7])
+        print('MQ7: ', measure[8])
+        print('===========================')
     time.sleep(1)
 
